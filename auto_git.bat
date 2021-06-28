64cee6592e0c6d36afd138b991329a27e288e9c0
yo2se2 <yo2se2study@gmail.com>
2021-06-28 22:03:58 +0900

    auto commit

--- auto_git.bat
+++ auto_git.bat
@@ -1,7 +1,7 @@
 cd C:\Users\yonezawa\github\atcoder
 git init
 git config --global user.name "yo2se2"
-git config --global user.email "yo2se2@gmail.com"
+git config --global user.email "yo2se2study@gmail.com"
 git clone https://github.com/yo2se2/atcoder C:\Users\yonezawa\github\atcoder\atcoder
 xcopy /d C:\Users\yonezawa\desktop\atcoder C:\Users\yonezawa\github\atcoder\atcoder
 cd atcoder
